0f9bff6c6cbff0ec60db8cacbde39736c3ed3391
Mateusz.Starczyk <mati.starczyk@gmail.com>
2019-11-01 19:06:14 +0100

    Added fields to edit card details

--- app/src/main/java/com/mateuszstarczyk/nfcopy/ui/new_card/NewCardFragment.java
+++ app/src/main/java/com/mateuszstarczyk/nfcopy/ui/new_card/NewCardFragment.java
@@ -35,17 +35,25 @@ public class NewCardFragment extends Fragment {
     private NewCardViewModel newCardViewModel;
     private NfcReader nfcReader;
     private NavController navController;
+    private View root;
+    private PulsatorLayout pulsator;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         newCardViewModel =
                 ViewModelProviders.of(this).get(NewCardViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_new_card, container, false);
+        root = inflater.inflate(R.layout.fragment_new_card, container, false);
 
-        PulsatorLayout pulsator = root.findViewById(R.id.pulsator);
-        pulsator.start();
+        pulsator = root.findViewById(R.id.pulsator);
+        if (newCardViewModel.isEditMode()) {
+            setEditMode();
+        } else {
+            pulsator.start();
+            setScanMode();
+        }
 
         navController = Navigation.findNavController(Objects.requireNonNull(getActivity()), R.id.nav_host_fragment);
+
 
         nfcReader = new NfcReader(getActivity(), new NfcAdapter.ReaderCallback() {
             @Override
@@ -53,19 +61,22 @@ public class NewCardFragment extends Fragment {
                 nfcReader.disableReaderMode();
                 Log.i("INFO", tag.toString());
 
-                TinyDB tinydb = new TinyDB(getContext());
-                ArrayList<NfcCard> tagsUIDs = tinydb.getListObject("nfc_cards", NfcCard.class);
-
-
-                ImageView imageView = new ImageView(getContext());
-                imageView.setImageDrawable(Objects.requireNonNull(getActivity()).getDrawable(R.drawable.ic_menu_card));
-                tagsUIDs.add(new NfcCard(byteToString(tag.getId()), String.valueOf(tagsUIDs.size()), Arrays.toString(tag.getTechList()), imageView));
-                tinydb.putListObject("nfc_cards", tagsUIDs);
+//                TinyDB tinydb = new TinyDB(getContext());
+//                ArrayList<NfcCard> tagsUIDs = tinydb.getListObject("nfc_cards", NfcCard.class);
+//
+//
+//                ImageView imageView = new ImageView(getContext());
+//                imageView.setImageDrawable(Objects.requireNonNull(getActivity()).getDrawable(R.drawable.ic_menu_card));
+//                tagsUIDs.add(new NfcCard(byteToString(tag.getId()), String.valueOf(tagsUIDs.size()), Arrays.toString(tag.getTechList()), imageView));
+//                tinydb.putListObject("nfc_cards", tagsUIDs);
 
                 getActivity().runOnUiThread(new Runnable() {
                     public void run() {
+                        setEditMode();
                         Toast.makeText(getContext(), R.string.action_card_added, Toast.LENGTH_SHORT).show();
-                        navController.navigate(R.id.nav_cards);
+
+
+//                        navController.navigate(R.id.nav_cards);
                     }
                 });
             }
@@ -73,6 +84,28 @@ public class NewCardFragment extends Fragment {
         nfcReader.enableReaderMode();
 
         return root;
+    }
+
+    private void setEditMode() {
+        newCardViewModel.setEditMode(true);
+        getActivity().runOnUiThread(new Runnable() {
+            public void run() {
+                pulsator.setVisibility(View.GONE);
+                root.findViewById(R.id.iv_scan).setVisibility(View.GONE);
+                root.findViewById(R.id.cl_new_card).setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+    private void setScanMode() {
+        newCardViewModel.setEditMode(false);
+        getActivity().runOnUiThread(new Runnable() {
+            public void run() {
+                root.findViewById(R.id.cl_new_card).setVisibility(View.GONE);
+                pulsator.setVisibility(View.VISIBLE);
+                root.findViewById(R.id.iv_scan). setVisibility(View.VISIBLE);
+            }
+        });
     }
 
     public void readTag(Tag tag){
--- app/src/main/java/com/mateuszstarczyk/nfcopy/ui/new_card/NewCardViewModel.java
+++ app/src/main/java/com/mateuszstarczyk/nfcopy/ui/new_card/NewCardViewModel.java
@@ -1,7 +1,5 @@
 package com.mateuszstarczyk.nfcopy.ui.new_card;
 
-import android.nfc.Tag;
-
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
@@ -9,13 +7,23 @@ import androidx.lifecycle.ViewModel;
 public class NewCardViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
+    private boolean isEditMode;
 
     public NewCardViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is scan new card fragment");
+        isEditMode = false;
     }
 
     public LiveData<String> getText() {
         return mText;
+    }
+
+    public boolean isEditMode() {
+        return isEditMode;
+    }
+
+    public void setEditMode(boolean editMode) {
+        isEditMode = editMode;
     }
 }
